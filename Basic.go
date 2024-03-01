8a919fbef32824bee44bbe87c2ef8ee16e14be3c
Aphichat0020 <65810266+Aphichat0020@users.noreply.github.com>
2024-03-01 14:41:29 +0700

    CRUD

--- Basic.go
+++ Basic.go
@@ -4,10 +4,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
 func main() {
 	app := fiber.New()
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!")
-	})
+
+	books = append(books, Book{ID: 1, Title: "1984", Author: "George Orwell"})
+	books = append(books, Book{ID: 2, Title: "The Great Gatsby", Author: "F. Scott Fitzgerald"})
+
+	app.Get("/book", getBooks)
+	app.Get("/book/:id", getBook)
+	app.Post("/book", createBook)
+	app.Put("/book/:id", updateBook)
+	app.Delete("/book/:id", deleteBook)
+
 	app.Listen(":8080")
 }
